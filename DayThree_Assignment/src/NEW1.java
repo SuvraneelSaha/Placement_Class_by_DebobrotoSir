3d4af37040317a22434fa115b213ca03ea5dd775
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-06-16 23:52:32 +0530

    Deleted all the codes for mock test

--- DayThree_Assignment/src/NEW1.java
+++
@@ -1,8 +0,0 @@
-public class NEW1 {
-    public static void main(String[] args) {
-
-    }
-}
-//1, 2, 1, 3, 2, 5, 3, 7, 5, 11, 8, 13, 13, 17
-// TCS mock interview question
-// prime + Fibonacci mix
