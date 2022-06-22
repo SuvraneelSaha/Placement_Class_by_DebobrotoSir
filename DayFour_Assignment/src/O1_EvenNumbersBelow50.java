a5bdc6ce1150b0514d534e230ca46cb09035e8f1
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-06-22 19:39:29 +0530

    Day 4 Assignment solvation started

---
+++ DayFour_Assignment/src/O1_EvenNumbersBelow50.java
@@ -0,0 +1,11 @@
+public class O1_EvenNumbersBelow50 {
+    public static void main(String[] args) {
+        for (int i = 1; i <50 ; i++)
+        {
+                if(i%2==0)
+                {
+                    System.out.print(i +" ");
+                }
+        }
+    }
+}
---
+++ DayFour_Assignment/src/O2_OddNumbersBelow40.java
@@ -0,0 +1,11 @@
+public class O2_OddNumbersBelow40 {
+    public static void main(String[] args) {
+        for (int i = 1; i <40 ; i++)
+        {
+            if(i%3==0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
