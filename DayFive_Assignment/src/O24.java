6b4713aecdb1df18ab096dec8bbddf727aad7b9f
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-06-27 00:34:28 +0530

    Upto 24 done no problems

--- DayFive_Assignment/src/O24.java
+++ DayFive_Assignment/src/O24.java
@@ -4,6 +4,17 @@ public class O24 {
     }
     static void Pattern24(int n)
     {
-
+        for (int row = n; row >=1 ; row--)
+        {
+            for (int i = 1; i <=row ; i++)
+            {
+                System.out.print("  ");
+            }
+            for (int col = n; col >=row ; col--)
+            {
+                System.out.print("*" +" ");
+            }
+            System.out.println();
+        }
     }
 }
