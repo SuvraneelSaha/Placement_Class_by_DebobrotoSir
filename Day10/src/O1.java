49cd4066ce64254126d3b14570e277f88d9592fd
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-07-16 15:02:48 +0530

    Day 10 and Day 10 Assignment Details added

---
+++ Day10/src/O1.java
@@ -0,0 +1,9 @@
+public class O1 {
+    public static void main(String[] args) {
+        System.out.println("Today's date - 16/07/22");
+        System.out.println("topics covered are - For Pattern , For Special Number Questions , Pre increment and Post increment ");
+        System.out.println("Questions done in Special Number -- magic number , kaprekar number and evil number ");
+        System.out.println("In patter question done - 31 32 from for pattern exercise ");
+        System.out.println("Post increment and pre increment follow copy ");
+    }
+}
---
+++ Day10_Assignment/src/O1.java
@@ -0,0 +1,5 @@
+public class O1 {
+    public static void main(String[] args) {
+        System.out.println("Today's date - 16/07/22");
+    }
+}
