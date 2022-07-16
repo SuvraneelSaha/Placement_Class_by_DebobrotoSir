7fdf8a84789d2458340361712760f4c067e89791
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-07-16 15:07:02 +0530

    Day 9 and Day 9 Assignment given

---
+++ Day9_Assignment/src/O1.java
@@ -0,0 +1,7 @@
+public class O1 {
+    public static void main(String[] args) {
+        System.out.println("Assignments");
+        System.out.println("1] Arrays basic Questions");
+        System.out.println("2] For Special Numbers ");
+    }
+}
