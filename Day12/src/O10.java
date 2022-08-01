516c8167c06854f2f4d8008a767c318431b341cf
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-08-01 10:50:37 +0530

    Update O10.java

--- Day12/src/O10.java
+++ Day12/src/O10.java
@@ -1,6 +1,6 @@
 public class O10 {
     public static void main(String[] args) {
-        // rotate of an array -- quite a important question 
+        // rotate of an array -- quite a important question
         // dont remember but use logic
         // logic build
 
