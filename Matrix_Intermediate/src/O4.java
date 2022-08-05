20e08815550d56dcb252c9018cb05ca8ee1fe6fc
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-08-06 01:58:49 +0530

    doing the 5

--- Matrix_Intermediate/src/O4.java
+++ Matrix_Intermediate/src/O4.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class O3 {
+public class O4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter the size of the row :");
@@ -26,21 +26,25 @@ public class O3 {
             System.out.println(Arrays.toString(row));
         }
 
-        int[][] brr = new int[n][m];
 
         for (int row = 0; row < n; row++)
         {
             for (int col = 0; col < m ; col++)
             {
-                brr[row][col] = arr[col][row];
+                if(row==0|| row==n-1 || col ==0||col ==m-1)
+                {
+
+                    System.out.print(arr[row][col] +"\t");
+                }
+                else
+                {
+
+                    System.out.print("\t");
+                }
 
             }
+            System.out.println();
+        }
 
-        }
-        System.out.println("New Array : ");
-        for (int[] row : brr)
-        {
-            System.out.println(Arrays.toString(row));
-        }
     }
 }
--- Matrix_Intermediate/src/O5.java
+++ Matrix_Intermediate/src/O5.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class O3 {
+public class O5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter the size of the row :");
@@ -25,22 +25,6 @@ public class O3 {
         {
             System.out.println(Arrays.toString(row));
         }
-
-        int[][] brr = new int[n][m];
-
-        for (int row = 0; row < n; row++)
-        {
-            for (int col = 0; col < m ; col++)
-            {
-                brr[row][col] = arr[col][row];
-
-            }
-
-        }
-        System.out.println("New Array : ");
-        for (int[] row : brr)
-        {
-            System.out.println(Arrays.toString(row));
-        }
+        
     }
 }
