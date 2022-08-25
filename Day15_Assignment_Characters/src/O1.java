5a071d2f17ce2e64afce51fd32c498ed8a743fe8
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-08-25 13:47:34 +0530

    1 done

---
+++ Day15_Assignment_Characters/src/O1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class O1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        char alphabet_A = 'A';
+        System.out.println(alphabet_A);
+        char alphabet_a = 'a';
+        System.out.println(alphabet_a);
+        char alphabet_E = 'E';
+        System.out.println(alphabet_E);
+        char alphabet_e = 'e';
+        System.out.println(alphabet_e);
+        char alphabet_I = 'I';
+        System.out.println(alphabet_I);
+        char alphabet_i = 'i';
+        System.out.println(alphabet_i);
+        char alphabet_O = 'O';
+        System.out.println(alphabet_O);
+        char alphabet_o = 'o';
+        System.out.println(alphabet_o);
+        char alphabet_U = 'U';
+        System.out.println(alphabet_U);
+        char alphabet_u = 'u';
+        System.out.println(alphabet_u);
+
+
+
+    }
+}
