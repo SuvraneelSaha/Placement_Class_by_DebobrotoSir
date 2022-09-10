b97cac71990c776cf19bb1da24c72526ff26b92a
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-09-10 14:31:18 +0530

    OOPS Comm Classroom Lecture 1 done

---
+++ OOPS Comm Classroom/src/Introduction/WrapperExample.java
@@ -0,0 +1,23 @@
+package Introduction;
+
+public class WrapperExample {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = 20 ;
+
+        System.out.println("After changing :");
+
+        Swap(a,b);
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        // still not passing due to the final keyword present in the Integer class 
+
+    }
+    static void Swap(Integer a , Integer b)
+    {
+        int temp = a ;
+        a=b;
+        b=temp;
+    }
+}
