9d8ae74bce58dec54dafefa03d71e8a52e3f75e3
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-09-13 16:14:23 +0530

    Method OverRdiding Done

---
+++ OOPS JavaTPoint/src/MethodOverRiding/O1_RulesForOverRiding.java
@@ -0,0 +1,12 @@
+package MethodOverRiding;
+
+public class O1_RulesForOverRiding {
+    /*
+
+    Rules for Java Method Overriding:
+
+    The method must have the same name as in the parent class
+    The method must have the same parameter as in the parent class.
+    There must be an IS-A relationship (inheritance).
+     */
+}
