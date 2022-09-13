c299e2dfa93becf600fb753641b94945a3898f24
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-09-13 23:15:00 +0530

    Package Done

---
+++ OOPS JavaTPoint/src/JavaPackage/O1_WhatIsJavaPackage.java
@@ -0,0 +1,21 @@
+package JavaPackage;
+
+public class O1_WhatIsJavaPackage {
+    /*
+    A java package is a group of similar types of classes, interfaces and sub-packages.
+
+Package in java can be categorized in two form, built-in package and user-defined package.
+
+There are many built-in packages such as java, lang, awt, javax, swing, net, io, util, sql etc.
+
+     */
+
+    /*
+    Advantage of Using a Java Package
+    1] Java package is used to categorize the classes and interfaces so that they can be easily maintained.
+    2]Java package provides access protection.
+    3] ava package removes naming collision.
+
+
+     */
+}
---
+++ OOPS JavaTPoint/src/JavaPackage/O2_Example_Of_Package.java
@@ -0,0 +1,21 @@
+package JavaPackage;
+
+public class O2_Example_Of_Package {
+    /*
+    Example of a java package
+    So We use ArrayList<> list = new ArrayList();
+    so the ArrayList is itself a class and we were making a new object list which is an instance of the Class
+    ArrayList
+
+    Now The ArrayList Class is present within the util package
+    the util is actually a sub package
+
+    the util package is a sub package which belongs to the Package java
+
+    so whenever we use Arraylist Class we need to need import
+
+    java.util.ArrayList;
+    
+
+     */
+}
