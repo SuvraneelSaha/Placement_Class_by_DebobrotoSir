4269605b338d9f9cd9c411cb426b12d0f0263e30
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-10-16 23:53:05 +0530

    Anagram question done

--- Day_23_Methods_Assignment/src/O16.java
+++ Day_23_Methods_Assignment/src/O16.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class O16 {
@@ -8,20 +9,65 @@ public class O16 {
         String word1 = "pears";
         String word2 = "spare";
 
+        char[] arr1 = Array(word1);
+
+        char[] arr2 = Array(word2);
+
+        System.out.println("Word Array 1 = "+ Arrays.toString(arr1));
+        System.out.println("Word Array 2 = "+Arrays.toString(arr2));
+
+        Arrays.sort(arr1);
+
+        Arrays.sort(arr2);
+
+        System.out.println("Is Anagram "+isAnagram(arr1,arr2));
+
+        
+
+
+
+
+
 
 
     }
-    public static boolean isAnagram(String word1,String word2)
+    public static Boolean isAnagram(char[] arr1 , char[] arr2)
     {
-        if(word1.length() != word2.length())
+        if(arr1.length != arr2.length)
+        {
+            return false;
+
+        }
+
+        int flag = 1;
+        for (int i = 0; i < arr1.length; i++)
+        {
+            if(arr1[i] != arr2[i])
+            {
+                flag = 0;
+                break;
+            }
+        }
+
+        if(flag==1)
+        {
+            return true;
+        }
+        else
         {
             return false;
         }
+    }
+    public static char[] Array(String word)
+    {
+        char[] arr = new char[word.length()];
 
-        for (int i = 0; i < word1.length(); i++)
+        for (int i = 0; i < word.length(); i++)
         {
-            if(word1.charAt(i))
+            arr[i] = word.charAt(i);
+
         }
+        return arr;
 
     }
 }
