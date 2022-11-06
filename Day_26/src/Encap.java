94388b565ce1371b7985703461fffaac0513526a
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-11-06 09:25:39 +0530

    day 26 abstraction and encapsulation

---
+++ Day_26/src/Encap.java
@@ -0,0 +1,16 @@
+public class Encap {
+    int x = 10;
+    private  int y = 20 ;
+    protected  int z = 30;
+    public int w = 40;
+
+    void meth()
+    {
+        System.out.println(y);
+
+    }
+}
+
+
+/// Math class use korte hobe
+// ceiling, floor ,
---
+++ Day_26/src/O1_encap_demo.java
@@ -0,0 +1,125 @@
+public class O1_encap_demo {
+    public static void main(String[] args) {
+
+        Encap obj = new Encap();
+        //System.out.println(obj.y);
+        System.out.println(obj.x);
+        System.out.println(obj.z);
+
+        System.out.println(obj.w);//public
+
+// package - is nothing but a folder
+        // encapsulation - data hiding using access specifiers
+        // public protected default private
+
+
+        /*
+        Difference between abstraction and encapsulation
+
+        abstraction - abstraction is used to present to the end user
+        ex - Bank balance to the end user
+        How you are doing , not needed
+        Focus on what it is doing
+
+        encapsulation - it is meant for the developers
+        Focus on what it is doing and also and how it is doing
+
+
+        Speak Less ,  Makes it Mysterious
+
+
+         */
+
+
+    }
+}
+
+
+    // inherit - subclass method
+    // object - non-subclass method
+
+
+    /*
+            Under Same Package
+                    same class          different class(sub class)          different class(not sub class)
+        private -       yes                 no                                      no
+        default -       yes                 yes                                     yes
+        protected -     yes                 yes                                     yes
+        public -        yes                 yes                                     yes
+
+        Under Different Package
+
+                        sub class       non subclass
+        private-             no             no
+        default -            no             no
+        protected-           yes            no
+        public-              yes            yes
+
+
+
+    for protected its only extends to inheritance of a different package
+    but when inheritance is not present then it wont - it will give error s
+
+
+for package - its just a folder
+ex -
+public repo;
+public class test
+{
+private int x = 10 ;
+public void getx()
+{
+sout(x)
+}
+// only it is used for printing
+// you cant change the value in the var
+
+
+
+import repo,*;
+
+class test_demo
+{
+psvm()
+    {
+        test obj = new test();
+        obj.getx();
+
+    }
+}
+
+in cmd
+now java test_demo.java
+java test_done
+
+explore , make networks - be cool minded
+and Belief
+
+
+5- conginzant 2 times visa rejected to usa
+1st time - geche thailand
+then dukkhe cognizant chere diyechilo
+then destiny
+
+5 yrs - IBM
+ibm er project chilo usa
+then jara juniors belgium e chole gelo
+sir puro frustated
+
+2020 - sir lucked open
+offer letter of ballys
+2021 - Accenture
+
+class kore jaa anonda sir er jaa joy its true
+
+Hospital e bose oo kaaj koreche
+
+
+aamader kaaj - Honestly kaaj kore jeo
+baas - ups and down aasche
+but aasbe aasbe thik hobe
+
+
+
+
+     */
