01dfb4f171df23c87f1af14fd3bd0a073c031421
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-04-17 23:44:30 +0530

    circle question done

--- Placement23Practice/src/BASIC/O5.java
+++ Placement23Practice/src/BASIC/O5.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 public class O5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        System.out.println("Enter the radius of the circle ");
+        if(in.hasNextFloat())
+        {
+            System.out.println("Yes");
+        }
+        float r = in.nextFloat();
+        double peri = 2 * 3.14 * r;
+        System.out.printf("Perimeter of the circle is %f ",peri);
+        System.out.println();
+        double area = 3.14 * 3.14 * r;
+        System.out.printf("Area = %f ",area);
     }
 }
